5844e51a3e2c3cef6f1016a0e1a21ef845ed03c9
Finn Thorwarth <mail@finnthorwarth.de>
2022-04-05 10:16:47 +0200

    Visuelle Fixes

--- src/views/pages/PageCars.vue
+++ src/views/pages/PageCars.vue
@@ -1,11 +1,23 @@
 <template>
-  <create-car-component />
+  <ion-page>
+    <Header :title="title" />
+    <ion-content :fullscreen="true">
+      <create-car-component />
+    </ion-content>
+  </ion-page>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import createCarComponent from "../components/createCarComponent.vue";
+import Header from "../components/HeaderComponent.vue";
+import { IonPage, IonContent } from "@ionic/vue";
 export default defineComponent({
-  components: { createCarComponent },
+  components: { createCarComponent, Header, IonPage, IonContent },
+  data() {
+    return {
+      title: "Fahrzeuge",
+    };
+  },
   methods: {},
 });
 </script>
